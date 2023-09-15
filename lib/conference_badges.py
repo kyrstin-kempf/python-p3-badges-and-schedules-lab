f5f4985a99b178d817ef1bd1affa551f8533a976
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-15 14:56:17 -0400

    created iterations and list comprehensions

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
+# import ipdb
+
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    # badge_list = []
+    # for n in names:
+    #     badge_list.append(badge_maker(n))
+    # return badge_list
+    
+    return [badge_maker(n) for n in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    rooms = range(1, 9)
 
-def printer(names):
-    return None
+    # room_assignments = []
+    # for r in rooms:
+    #     room_assignments.append(f"Hello, {speakers[r - 1]}! You'll be assigned to room {r}!")
+    # return room_assignments
+    
+    return [f"Hello, {speakers[r - 1]}! You'll be assigned to room {r}!" for r in rooms]
+
+def printer(searched_names):
+    for badge in batch_badge_creator(searched_names):
+        print(badge)
+    for assignment in assign_rooms(searched_names):
+        print(assignment)
+
+# ipdb.set_trace()
